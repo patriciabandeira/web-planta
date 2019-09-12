3e3eb7fa8761a6bb2319bb9a7b99a9eef2b92987
Patricia Bandeira <patriciaband2001@gmail.com>
2019-09-11 20:11:41 -0400

    Inclusao de dados estatisticos no dashboard.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Planta;
+use App\Bioma;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,10 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $qtd = [
+            'plantas' => Planta::count(),
+            'biomas' => Bioma::count()
+        ];
+        return view('home', compact('qtd'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,50 @@
 @section('title', 'Início')
 
 @section('content_header')
-    <h1>Dashboard</h1>
+    <h1>Início</h1>
 @stop
 
 @section('content')
-    <p>You are logged in!</p>
+<div class="row">
+    <div class="col-lg-3 col-xs-6">
+        <!-- small box -->
+        <div class="small-box bg-green">
+            <div class="inner">
+                <h3>{{ $qtd['plantas'] }}</h3>
+                <p>Plantas</p>
+            </div>
+            <div class="icon">
+                <i class="ion ion-leaf"></i>
+            </div>
+        </div>
+    </div>
+    <!-- ./col -->
+    <div class="col-lg-3 col-xs-6">
+        <!-- small box -->
+        <div class="small-box bg-aqua">
+            <div class="inner">
+                <h3>{{ $qtd['biomas'] }}</h3>
+                <p>Biomas</p>
+            </div>
+            <div class="icon">
+                <i class="ion ion-android-globe"></i>
+            </div>
+        </div>
+    </div>
+    <!-- ./col -->
+</div>
+
+<div class="box">
+    <div class="box-header with-border">
+        <h3 class="box-title">Boas Vindas</h3>
+    </div>
+    <div class="box-body">
+        Seja bem-vindo ao sistema Mundo das Plantas.
+    </div>
+    <!-- /.box-body -->
+    <div class="box-footer">
+         
+    </div>
+    <!-- /.box-footer-->
+</div>
 @stop
